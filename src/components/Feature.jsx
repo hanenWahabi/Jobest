661e5269e1e86b1b12d84f59334b5c139fc1f9f9
khouildianware <khouildi.anware@gmail.com>
2020-04-04 21:08:10 +0200

    'footer'

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,64 +1,64 @@
 import React from "react";
 function Feature(props) {
-  return (
-  <div className={props.type==='inverse'? "wrapperInverse"    :"wrapper" }>
+return (
+<div className={props.type==='inverse' ? "wrapperInverse" :"wrapper" }>
   <div className="left">
-  {props.type==='inverse'?
-  <div className="content">
-  <p className="title">{props.title}</p>
-  <p className="description">{props.description}</p>
-</div>
-:
-<div className="justifyPicture">
-<img src={props.srcPicture} className="pictureLeft" alt="logo" />
-</div>
-  }
-        </div>
-        <div className="right">
-        {props.type==='inverse'?  
-        <div className="justifyPicture">
-                  <img src={props.srcPicture} className="picture" alt="logo" />
+    {props.type==='inverse'?
+    <div className="content">
+      <p className="title">{props.title}</p>
+      <p className="description">{props.description}</p>
+    </div>  
+    :
+    <div className="justifyPicture">
+      <img src={props.srcPicture} className="pictureLeft" alt="logo" />
+    </div>
+    }
+  </div>
+  <div className="right">
+    {props.type==='inverse'?
+    <div className="justifyPicture">
+      <img src={props.srcPicture} className="picture" alt="logo" />
 
-           </div>
-  :
+    </div>
+    :
 
-<div className="content">
-<p className="title">{props.title}</p>
-<p className="description">{props.description}</p>
+    <div className="content">
+      <p className="title">{props.title}</p>
+      <p className="description">{props.description}</p>
+    </div>
+    }
+
+  </div>
 </div>
-}
-       
-          </div>
-        </div>
 
-      
-  );
+
+);
 }
 
 export default Feature;
 /*
-    <div className="wrapper">
-      <div className="one">
-        {props.type === "inverse" ? (
-          <div className="content">
-            <p className="title">{props.title}</p>
-            <p className="description">{props.description}</p>
-          </div>
-        ) : (
-          <img src={props.srcPicture} className="picture" alt="logo" />
-        )}
-      </div>
-      <div className="two">
-        {props.type == "inverse" ? (
-          <img src={props.srcPicture} className="picture" alt="logo" />
-        ) : (
-          <div className="content">
-            <p className="title">{props.title}</p>
-            <p className="description">{props.description}</p>
-          </div>
-        )}
-      </div>
+<div className="wrapper">
+  <div className="one">
+    {props.type === "inverse" ? (
+    <div className="content">
+      <p className="title">{props.title}</p>
+      <p className="description">{props.description}</p>
     </div>
+    ) : (
+    <img src={props.srcPicture} className="picture" alt="logo" />
+    )}
+  </div>
+  <div className="two">
+    {props.type == "inverse" ? (
+    <img src={props.srcPicture} className="picture" alt="logo" />
+    ) : (
+    <div className="content">
+      <p className="title">{props.title}</p>
+      <p className="description">{props.description}</p>
+    </div>
+    )}
+  </div>
+</div>
 
 
 
